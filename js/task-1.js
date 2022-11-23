70ef12fa79b323472ac4d79940a65c22ccb3cea8
Alinchyk <alina.mykhalchuk09@gmail.com>
2022-11-23 15:39:00 +0100

    add work version

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,39 @@
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
 const isLoginValid = function (login) {
-  // твой код
+  // if (login.lenght >= 4 && login.length <= 16) {
+  //   console.log("Ошибка! Логин должен быть от 4 до 16 символов");
+  //   return false;
+  // }
+  // return true;
+
+  return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function (allLogins, login) {
-  // твой код
+  // if (allLogins.includes(login)) {
+  //   console.log("Такой логин уже используется!");
+  //   return false;
+  // }
+  // return true;
+
+  return !allLogins.includes(login);
 };
 
 const addLogin = function (allLogins, login) {
-  // твой код
+  // if (isLoginValid(login) && isLoginUnique(allLogins, login)) {
+  //   allLogins.push(login);
+  //   console.log("Логин успешно добавлен!");
+  // }
+
+  if (!isLoginValid(login)) {
+    return "Ошибка! Логин должен быть от 4 до 16 символов";
+  } else if (!isLoginUnique(allLogins, login)) {
+    return "Такой логин уже используется!";
+  } else {
+    logins.push(login);
+    return "Логин успешно добавлен!";
+  }
 };
 
 /*
